6302aad9a52e3da9e614904a8a8f49cfb3aeca10
timmy471 <ayantunjitimilehin@gmail.com>
2024-11-13 11:55:56 +0100

    Added language direction

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import i18next from "i18next";
+import { useTranslation } from "react-i18next";
 
 const LanguageSelector = () => {
   // Set the initial language from i18next's detected or default language
   const [language, setLanguage] = useState(i18next.language);
+
+  const { i18n } = useTranslation();
 
   const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedLanguage = e.target.value;
     setLanguage(selectedLanguage);
     i18next.changeLanguage(selectedLanguage); // Update language in i18next
   };
+
+  useEffect(() => {
+    document.body.dir = i18n.dir(); //sets the body to ltr or rtl
+  }, [i18n, i18n.language]);
 
   return (
     <select
@@ -24,6 +31,7 @@ const LanguageSelector = () => {
       <option value="de">German</option>
       <option value="es">Spanish</option>
       <option value="yo">Yoruba</option>
+      <option value="ar">Arabic</option>
     </select>
   );
 };
