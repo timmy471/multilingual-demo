9da32be8e40fe98c6bb3e948db2abac4b57f7bc8
timmy471 <ayantunjitimilehin@gmail.com>
2024-11-13 12:27:50 +0100

    Added locales for expansion

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -6,6 +6,8 @@ import "./i18n.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <App />
+    </React.Suspense>
   </StrictMode>
 );
